c1e416624f0523fc395721722285ae58e6c4cdc0
Gael Varoquaux <gael.varoquaux@normalesup.org>
2024-11-19 11:34:04 +0100

    landing page

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,7 +5,8 @@ from __future__ import unicode_literals
 AUTHOR = u'Ga\xebl Varoquaux'
 SITENAME = u'Ga\xebl Varoquaux'
 SITEURL = 'https://gael-varoquaux.info'
-#SITEURL = 'http://localhost:8000/'
+# The below gets overiden in the publishconf.py
+SITEURL = 'http://localhost:8000/'
 AUTHOR_EMAIL = 'gael.varoquaux@normalesup.org'
 GITHUB_URL = 'https://github.com/GaelVaroquaux/'
 TWITTER_USERNAME = 'GaelVaroquaux'
